b01b9026ecb3da3789145cea74524fe96c44f7d8
Mohammad Hammad <hammad.scripted@gmail.com>
2026-07-18 15:33:27 +0530

    path variable added

--- src/main/java/net/javaguides/spring_boot_rest_api/controller/StudentController.java
+++ src/main/java/net/javaguides/spring_boot_rest_api/controller/StudentController.java
@@ -3,6 +3,7 @@ package net.javaguides.spring_boot_rest_api.controller;
 import net.javaguides.spring_boot_rest_api.bean.Student;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,9 +22,16 @@ public class StudentController {
     public List<Student> getStudentList() {
 
         List<Student> studentList = new ArrayList<Student>();
-       studentList.add(new Student(1, "Mohammad", "Hammad"));
-       studentList.add(new Student(2, "Mohammad", "Adnaan"));
-       studentList.add(new Student(3, "Mohammad", "Fazil"));
+        studentList.add(new Student(1, "Mohammad", "Hammad"));
+        studentList.add(new Student(2, "Mohammad", "Adnaan"));
+        studentList.add(new Student(3, "Mohammad", "Fazil"));
         return studentList;
     }
+
+    //    ? path variable
+    @GetMapping("student/{id}")
+    public Student getStudentById(@PathVariable Integer id) {
+       return   new Student(id, "Mohammad", "Hammad");
+    }
+
 }
