1323229fef68fcba482aafd1b9dbfb09d5ce8bdf
Mohammad Hammad <hammad.scripted@gmail.com>
2026-07-18 15:25:44 +0530

    list of student added

--- src/main/java/net/javaguides/spring_boot_rest_api/controller/StudentController.java
+++ src/main/java/net/javaguides/spring_boot_rest_api/controller/StudentController.java
@@ -5,13 +5,25 @@ import net.javaguides.spring_boot_rest_api.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
     @GetMapping("student")
-    public Student getStudent()
-    {
+    public Student getStudent() {
 
         return new Student(1, "Mohammad", "Hammad");
+    }
+
+    @GetMapping("list")
+    public List<Student> getStudentList() {
+
+        List<Student> studentList = new ArrayList<Student>();
+       studentList.add(new Student(1, "Mohammad", "Hammad"));
+       studentList.add(new Student(2, "Mohammad", "Adnaan"));
+       studentList.add(new Student(3, "Mohammad", "Fazil"));
+        return studentList;
     }
 }
