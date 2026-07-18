4dfa1ea5f0d7d7dc43d4af6ebf8c70ed947896e0
Mohammad Hammad <hammad.scripted@gmail.com>
2026-07-18 16:00:02 +0530

    put method added

--- src/main/java/net/javaguides/spring_boot_rest_api/controller/StudentController.java
+++ src/main/java/net/javaguides/spring_boot_rest_api/controller/StudentController.java
@@ -2,10 +2,8 @@ package net.javaguides.spring_boot_rest_api.controller;
 
 import net.javaguides.spring_boot_rest_api.bean.Student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +37,23 @@ public class StudentController {
     @GetMapping("student/query")
     public Student studentRequestVariable(@RequestParam Integer id, @RequestParam String firstName, @RequestParam String lastName) {
         return new Student(id, firstName, lastName);
+
+    }
+
+    @PostMapping("student/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+    @PutMapping("student/{id}/update")
+    public Student updateStudent(@RequestBody Student student, @PathVariable("id") Integer studentId) {
+
+        student.setId(studentId);
+        return student;
 
     }
 
